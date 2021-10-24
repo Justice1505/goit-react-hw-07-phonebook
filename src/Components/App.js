c83de1d5cecfd29eb14e956deb0febf2226e7004
Alexander Osadchuk <85343271+Justice1505@users.noreply.github.com>
2021-10-24 22:22:55 +0300

    some fixes

--- src/Components/App.js
+++ src/Components/App.js
@@ -18,13 +18,9 @@ const App = () => {
   return (
     <div className="block">
       <h1 className={s.title}>Phonebook</h1>
-
       <ContactForm />
-
       <h2 className={s.title}>Contacts</h2>
-
       <Filter />
-
       <ContactList />
     </div>
   );
